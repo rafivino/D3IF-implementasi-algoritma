549e9296c7a0a7b058fbd6ad6eef198ec0dbe222
rafivino <rafiesudrajat11@gmail.com>
2021-10-04 20:50:15 +0700

    Tugas2.java

---
+++ Praktikum/Tugas2.java
@@ -0,0 +1,17 @@
+public class Tugas2 {
+
+	public static void main (String[] args) {
+	
+		double IPK=3.0;
+		String nama="Rafie Novianto Sudrajat";
+		long NIM=6706213142L;
+		
+			System.out.println("IPK=" + IPK);
+			System.out.println("nama=" + nama);
+			System.out.println("NIM=" + NIM);
+	
+	
+	
+	}
+
+}
